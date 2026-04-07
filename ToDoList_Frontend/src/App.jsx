fa0285d8f5115bf7bccdb240a1a928eed8a62f3f
Claire Vaugarny <claire.vaugarny@laposte.net>
2026-04-07 18:01:21 +0200

    complément de la transition

--- ToDoList_Frontend/src/App.jsx
+++ ToDoList_Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Test from "./pages/Test";
+import Home from "./pages/Home.jsx";
+import Test from "./pages/Test.jsx";
 
 function App() {
   return (
